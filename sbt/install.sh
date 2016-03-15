5fc3715fe715e32c522e5b4381d6155788d0afbe
dave.yxw <dave.yxw@gmail.com>
2016-03-15 21:51:58 +0800

    add sbt scripts

---
+++ sbt/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#wget https://repo.typesafe.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.11/sbt-launch.jar
+
+
+mkdir -p ~/bin/
+
+mv sbt-launch.jar ~/bin/
+cp sbt ~/bin/
+
+
+chmod u+x ~/bin/sbt
