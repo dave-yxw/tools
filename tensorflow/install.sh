681cc117e0ab9ae43225aaa0c31b81377f3fcf7f
xiaowei.yan <dave.yxw@gmail.com>
2017-04-02 15:29:34 +0800

    add tf install

---
+++ tensorflow/install.sh
@@ -0,0 +1,15 @@
+./configure
+
+bazel build -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-mfpmath=both --copt=-msse4.2 --config=cuda //tensorflow/cc:tutorials_example_trainer
+
+bazel-bin/tensorflow/cc/tutorials_example_trainer --use_gpu | head
+
+bazel build -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-mfpmath=both --copt=-msse4.2 --config=cuda //tensorflow/tools/pip_package:build_pip_package
+
+bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+
+# .whl 文件的实际名字与你所使用的平台有关
+pip install `ls /tmp/tensorflow_pkg/tensorflow-*.whl`
+
+
+
